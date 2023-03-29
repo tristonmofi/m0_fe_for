2f330df9ab5f0f63e69d5161df462d703144ea37
Triston Modlin-Filippi <127793213+tristonmofi@users.noreply.github.com>
2023-03-29 16:35:09 -0600

    add changes

--- for.js
+++ for.js
@@ -20,7 +20,7 @@ for(var i = 0; i < weather_options.length; i++) {
 // Use a for statement to log to the console only the words that are exactly 3 characters in length
 var words = ["car", "bike", "bus", "van", "scooter", "truck", "rv"]
 
-for(var i = 0; i < words.length; i+=3) {
+for(var i = 0; i = words.length; i+=3) {
   console.log("words: " + words[i])
 }
 
