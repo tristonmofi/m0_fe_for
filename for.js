f77005a3f225d0640791ef71b7e76004d736a3a9
Triston Modlin-Filippi <127793213+tristonmofi@users.noreply.github.com>
2023-03-29 16:33:52 -0600

    add changes

--- for.js
+++ for.js
@@ -3,17 +3,26 @@
   
 // Challenge 1
 // Use a for statement to log to the console all even numbers between 25 and 75.
-
+for(var i = 26; i < 75; i+=2) {
+  console.log('Count', i);
+}
 
 
 // Challenge 2
 // Use a for statement to log to the console a complete sentence stating the weather using the Array below:
 var weather_options = ["sunny", "snowy", "freezing", "partly cloudy", "drizzly"]
 
+for(var i = 0; i < weather_options.length; i++) {
+  console.log("Today the weather will be " + weather_options[i]);
+}
 
 // Challenge 3
 // Use a for statement to log to the console only the words that are exactly 3 characters in length
 var words = ["car", "bike", "bus", "van", "scooter", "truck", "rv"]
+
+for(var i = 0; i < words.length; i+=3) {
+  console.log("words: " + words[i])
+}
 
 // 📹 Record yourself live-coding and talking through any ONE of the challenges. 
   // The recording doesn't have to be the first time you are solving the problem. 
